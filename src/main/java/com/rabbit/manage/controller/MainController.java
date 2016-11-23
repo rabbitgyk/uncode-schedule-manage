0dc0b514355fb9defce69956d55c086e6ec1eab9
guoyankui <yankuiguo@gmail.com>
2016-11-23 22:13:48 +0800

    add spring mvc

---
+++ src/main/java/com/rabbit/manage/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.rabbit.manage.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+	private Logger log = LoggerFactory.getLogger(MainController.class);
+
+	@RequestMapping("/name")
+	@ResponseBody
+	public String name(String name){
+		log.info("name = " + name);
+		return "abcv" + name;
+	}
+}
